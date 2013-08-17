bf0414d8a3256690abd077239a819eed37ef7122
Matt Hampel <matthew.hampel@gmail.com>
2013-08-17 12:31:20 -0400

    Broken orgs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,8 @@
 class Organization < ActiveRecord::Base
+  include ActiveModel::Serialization
+
   extend FriendlyId
+
   attr_accessible :description, :name, :slug, :type, :grants_received, :grants_made
 
   friendly_id :name, use: :slugged
@@ -9,12 +12,16 @@ class Organization < ActiveRecord::Base
 
   validates :name, :uniqueness => true
 
-  def grants_received_total
+  def total_grants_received
     grants_received.sum(:amount)
   end
 
-  def grants_made_total
+  def total_grants_made
     grants_made.sum(:amount)
+  end
+
+  def serializable_hash(options={})
+    super.serializable_hash options.merge(methods: [:total_grants_received, :total_grants_made])
   end
 
 end
